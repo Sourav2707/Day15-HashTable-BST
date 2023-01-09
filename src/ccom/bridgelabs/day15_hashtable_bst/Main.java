b72dbdbc7eb1baafa44f0e3c4c93a216c1c5f9f1
Sourav2707 <smartsourav33@gmail.com>
2023-01-09 18:24:23 +0530

    Initial commit with welcome message to day 15 hashtable and bst program

--- src/ccom/bridgelabs/day15_hashtable_bst/Main.java
+++ src/ccom/bridgelabs/day15_hashtable_bst/Main.java
@@ -1,6 +1,6 @@
 package ccom.bridgelabs.day15_hashtable_bst;
 
-public class CustomHashTable {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to day 15 hashtable and binary search tree program");
     }
