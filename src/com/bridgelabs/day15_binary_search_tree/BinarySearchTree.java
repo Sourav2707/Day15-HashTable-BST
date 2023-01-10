c1f8b0d0aa35f6563eeb489fec988e72859a25cd
Sourav2707 <smartsourav33@gmail.com>
2023-01-10 22:19:03 +0530

    UC3 Ability to search 63 in the Binary Tree

--- src/com/bridgelabs/day15_binary_search_tree/BinarySearchTree.java
+++ src/com/bridgelabs/day15_binary_search_tree/BinarySearchTree.java
@@ -3,29 +3,41 @@ package com.bridgelabs.day15_binary_search_tree;
 public class BinarySearchTree<K extends Comparable> {
     MyBinaryNode<K> root;
 
-    public void add(K key) {
-        MyBinaryNode myBinaryNode = new MyBinaryNode(key);
-        if (root == null) {
-            root = myBinaryNode;
-            System.out.println(myBinaryNode.getKey()+" assigned to root");
+    public void add(K key){
+        addRecursive(key, root);
+    }
+    public void addRecursive(K key, MyBinaryNode currentNode){
+        if (root == null){
+            root=new MyBinaryNode(key);
+            System.out.println(key+" assigned to root");
+        } else if (currentNode.left==null && key.compareTo(currentNode.getKey()) <= 0) {
+            currentNode.left=new MyBinaryNode(key);
+            System.out.println(key+" assigned left to "+currentNode.getKey());
+        } else if (key.compareTo(currentNode.getKey()) <= 0) {
+            currentNode=currentNode.left;
+            addRecursive(key,currentNode);
+        } else if (currentNode.right == null) {
+            currentNode.right=new MyBinaryNode(key);
+            System.out.println(key+" assigned right to "+currentNode.getKey());
         } else {
-            MyBinaryNode currentNode=root;
-
-            while (true){
-                if (currentNode.left==null && key.compareTo(currentNode.getKey()) <= 0) {
-                    currentNode.left=new MyBinaryNode(key);
-                    System.out.println(key+" assigned left to "+currentNode.getKey());
-                    break;
-                } else if (key.compareTo(currentNode.getKey()) <= 0) {
-                    currentNode=currentNode.left;
-                } else if (currentNode.right == null) {
-                    currentNode.right=new MyBinaryNode(key);
-                    System.out.println(key+" assigned right to "+currentNode.getKey());
-                    break;
-                } else {
-                    currentNode = currentNode.right;
-                }
-            }
+            currentNode=currentNode.right;
+            addRecursive(key,currentNode);
         }
+    }
+    public MyBinaryNode<K> search(K key){
+        return search_recursive(key,root);
+    }
+    public MyBinaryNode<K> search_recursive(K key, MyBinaryNode<K> currentNode){
+        if (currentNode.getKey().equals(key)){
+            System.out.println("found: "+currentNode.getKey());
+            return currentNode;
+        } else if (currentNode.getKey().compareTo(key) > 0) {
+            currentNode=currentNode.left;
+            search_recursive(key,currentNode);
+        }else {
+            currentNode=currentNode.right;
+            search_recursive(key,currentNode);
+        }
+        return currentNode;
     }
 }
