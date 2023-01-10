250c35d39ed58a932d42bb3677747d7b960af032
Sourav2707 <smartsourav33@gmail.com>
2023-01-10 22:17:04 +0530

    UC2 Ability to create the binary tree shown in the figure

--- src/com/bridgelabs/day15_hashtable_bst/Main.java
+++ src/com/bridgelabs/day15_hashtable_bst/Main.java
@@ -3,24 +3,17 @@ package com.bridgelabs.day15_hashtable_bst;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to day 15 hashtable and binary search tree program");
-            MyHashMap<String,Integer> myHashmap=new MyHashMap<>();
-<<<<<<< HEAD
-            String sentence="Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
-=======
-            String sentence="To be or not to be";
->>>>>>> UC1_BST_Adding
-            String[] wordArray=sentence.split(" ");
-            System.out.println("The frequency of words in the given sentence 'To be or not to be as follows'");
-            for (String word : wordArray) {
-                myHashmap.put(word, 1);
-            }
-            myHashmap.print();
-<<<<<<< HEAD
+        MyHashMap<String,Integer> myHashmap=new MyHashMap<>();
+        String sentence="Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+        String[] wordArray=sentence.split(" ");
+        System.out.println("The frequency of words in the given sentence 'To be or not to be as follows'");
+        for (String word : wordArray) {
+            myHashmap.put(word, 1);
+        }
+        myHashmap.print();
         System.out.println("\n");
-            myHashmap.remove("avoidable");
+        myHashmap.remove("avoidable");
         System.out.println("");
-            myHashmap.print();
-=======
->>>>>>> UC1_BST_Adding
+        myHashmap.print();
     }
 }
