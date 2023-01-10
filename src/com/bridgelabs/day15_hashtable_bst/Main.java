ea93c30d9b08aff62547156feb7b8d3996363111
Sourav2707 <smartsourav33@gmail.com>
2023-01-10 13:40:05 +0530

    UC2 Ability to find frequency of words in a large paragraph

--- src/com/bridgelabs/day15_hashtable_bst/Main.java
+++ src/com/bridgelabs/day15_hashtable_bst/Main.java
@@ -3,5 +3,13 @@ package com.bridgelabs.day15_hashtable_bst;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to day 15 hashtable and binary search tree program");
+            MyHashMap<String,Integer> myHashmap=new MyHashMap<>();
+            String sentence="Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            String[] wordArray=sentence.split(" ");
+            System.out.println("The frequency of words in the given sentence 'To be or not to be as follows'");
+            for (String word : wordArray) {
+                myHashmap.put(word, 1);
+            }
+            myHashmap.print();
     }
 }
